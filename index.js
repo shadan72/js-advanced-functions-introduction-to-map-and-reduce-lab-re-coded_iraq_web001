a4eee2acec7930a92760efab6247f335fe20e3aa
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-09 19:49:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const mapToSquare=function(Alist){
 
 const reduceToTotal= function(Alist, val=0){
   
-  let total=Alist.reduce((x,val) => x + val);
+  let total=Alist.reduce((x,val) => x + val,val);
   return total;
 }
 

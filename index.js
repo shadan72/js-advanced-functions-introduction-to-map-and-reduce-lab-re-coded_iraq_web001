89d437b4c428cd62e9d77732ea721b322189a5df
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-09 19:05:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Your code here
 
-const Alist=[1,2,5,7,8]
+const Alist=[1,2,5,7,8];
 const mapToNegativize=function(Alist){
   const Negative= Alist.map(x => x=-x);
   return Negative;
-}
+};
 
 const mapToNoChange= function(Alist){
-  const NoChange=Alist.map(x => x=x)
+  const NoChange=Alist.map(x => x=x);
   return NoChange;
-}
+};
